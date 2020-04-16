30ac208d0f91e8c23f41436dff57348d3d1a5bcb
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-17 01:58:02 +0800

    Meta tweaks

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,12 +3,12 @@ import termImg = require('.');
 import {UnsupportedTerminalError} from '.';
 
 expectType<void>(termImg('/foo/bar.jpg'));
-expectType<void>(termImg(new Buffer(1)));
+expectType<void>(termImg(Buffer.from(1)));
 expectType<void>(termImg('/foo/bar.jpg', {width: 1}));
 expectType<void>(termImg('/foo/bar.jpg', {fallback: () => false}));
 
 expectType<string>(termImg.string('/foo/bar.jpg'));
-expectType<string>(termImg.string(new Buffer(1)));
+expectType<string>(termImg.string(Buffer.from(1)));
 expectType<string>(termImg.string('/foo/bar.jpg', {width: 1}));
 expectType<string | boolean>(
 	termImg.string('/foo/bar.jpg', {fallback: () => false})

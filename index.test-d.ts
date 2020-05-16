141a0c1ddd79601ab1471ccddc30c8a174499c04
Richie Bendall <richiebendall@gmail.com>
2020-05-16 22:19:23 +1200

    Don't do unrelated changes

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,9 +4,9 @@ import {UnsupportedTerminalError} from '.';
 
 expectType<string>(termImg('/foo/bar.jpg'));
 expectType<string>(termImg(Buffer.from(1)));
-expectType<string>(termImg('/foo/bar.jpg', { width: 1 }));
+expectType<string>(termImg('/foo/bar.jpg', {width: 1}));
 expectType<string | boolean>(
-	termImg('/foo/bar.jpg', { fallback: () => false })
+	termImg('/foo/bar.jpg', {fallback: () => false})
 );
 
 const unsupportedTerminalError = new UnsupportedTerminalError();
